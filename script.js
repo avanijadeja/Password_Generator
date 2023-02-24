becf2ffb68e67adbbc3b94d5e9a24bb6d8332bd0
Avani <avani.jadeja@gmail.com>
2023-02-24 15:48:39 -0600

    Add variables and arrays

---
+++ script.js
@@ -0,0 +1,31 @@
+// decalre variables and arrays and its values
+
+var characterLength = 8;
+var choiceArr =[];
+
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')','{','}','[',']','|','?','/','<','>',':',';','=','`','~','-','+','_','.',',','"'];
+var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0',];
+
+
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
